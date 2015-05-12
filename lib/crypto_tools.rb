a2e430186430288cb28d7b276e8df66c7fe9c6c1
David Bjergaard <dbjergaard@gmail.com>
2015-05-12 12:30:15 -0400

    Challenges 1-4 work!

--- lib/crypto_tools.rb
+++ lib/crypto_tools.rb
@@ -30,8 +30,9 @@ class CryptoTools
   end
   def self.freq_hist(test_str)
     hist = Hash.new(0)
-    total=test_str.length
-    test_str.each_char {|c| 
+    sani_str=test_str.scan(/[A-Za-z ]/).join.downcase
+    total=sani_str.length
+    sani_str.each_char {|c| 
       hist[c]+=1
     }
     hist.each_key{ |k|
@@ -41,32 +42,40 @@ class CryptoTools
   end
   def self.pearson_chi2(observed,expected)
     chi2=0
-    if observed.keys.length == 0
-      return -1
-    end
     expected.keys.each{ |c| 
-      chi2+=(((observed[c]-expected[c])**2)/expected[c])
+      val=(observed[c]-expected[c])**2/expected[c]
+      # puts sprintf "%s: \t Obs: %.3g \t Exp: %.3g \t QuadDiff: %.3g \t ChiTerm: %.3g ",c,observed[c],expected[c],(observed[c]-expected[c])**2,val
+      chi2+=val
     }
     chi2
   end
   def self.build_hist(file_name)
     file=File.open(file_name)
     length=0
-    freq_hist=Hash.new(0)
+    hist=Hash.new(0)
     file.each_line do |l| 
-      length+=l.length
-      l.scan(/[[A-Za-z0-9 ]]/).join.each_char do |c| 
-        freq_hist[c]+=1
+      stripped=l.scan(/[[A-Za-z ]]/).join
+      length+=stripped.length
+      stripped.each_char do |c| 
+        hist[c]+=1
       end
     end
-    freq_hist.each_key do |k| 
-      freq_hist[k]/=Float(length)
+    hist.each_key do |k| 
+      hist[k]/=Float(length)
     end
   end
   def self.score_string(raw_string)
-    english_freq={"P"=>0.0010267737217562606, "r"=>0.03819359460346835, "o"=>0.05518311792973764, "j"=>0.001325254454824941, "e"=>0.09002775870817539, "c"=>0.016804465271766707, "t"=>0.06942064889711369, " "=>0.16693430439065157, "G"=>0.0011521356296451063, "u"=>0.023084499895531744, "n"=>0.046974897770348925, "b"=>0.00967674536608662, "g"=>0.016416440318777422, "s"=>0.04166194072172641, "A"=>0.004304092170850372, "l"=>0.030164462883920842, "i"=>0.04657493358803689, "d"=>0.03120317583499985, "v"=>0.005438318956511357, "W"=>0.0015282213533116438, "a"=>0.05421007073993374, "y"=>0.014577799003074352, "L"=>0.0009431991164970302, "w"=>0.01609408112706325, "C"=>0.0011043787123541176, "T"=>0.0034086499716443302, "h"=>0.04524967913321195, "B"=>0.0007462018326717011, "k"=>0.007175476822971078, "f"=>0.013419693758767871, "m"=>0.01340178491478375, "Y"=>0.0008476852819150524, "p"=>0.010721427931827001, "D"=>0.0014446467480524131, "J"=>7.760499059785691e-05, "2"=>7.16353759364833e-05, "5"=>7.760499059785691e-05, "0"=>0.00013133152255021937, "8"=>5.9696146613736084e-05, "E"=>0.0018684893890099394, "1"=>0.0004059337969734054, "R"=>0.001265558308211205, "M"=>0.001325254454824941, "9"=>6.566576127510968e-05, "4"=>5.9696146613736084e-05, "S"=>0.0017192490224755992, "O"=>0.0013968898307614244, "F"=>0.0007999283646240636, "H"=>0.001844610930364445, "I"=>0.004960749783601469, "U"=>0.0006626272274124705, "N"=>0.0010864698683699966, "K"=>0.0005253260902008775, "V"=>0.00031041996239142766, "3"=>7.760499059785691e-05, "q"=>0.0008058979792854371, "x"=>0.0010148344924335135, "Z"=>5.9696146613736086e-06, "z"=>0.0004715995582485151, "Q"=>0.0005074172462167568, "X"=>3.581768796824165e-05, "7"=>3.581768796824165e-05, "6"=>4.178730262961526e-05}
+    english_freq={"a" => 0.0651738, "b" => 0.0124248, "c" => 0.0217339,
+                  "d" => 0.0349835, "e" => 0.1041442, "f" => 0.0197881,
+                  "g" => 0.0158610, "h" => 0.0492888, "i" => 0.0558094,
+                  "j" => 0.0009033, "k" => 0.0050529, "l" => 0.0331490,
+                  "m" => 0.0202124, "n" => 0.0564513, "o" => 0.0596302,
+                  "p" => 0.0137645, "q" => 0.0008606, "r" => 0.0497563,
+                  "s" => 0.0515760, "t" => 0.0729357, "u" => 0.0225134,
+                  "v" => 0.0082903, "w" => 0.0171272, "x" => 0.0013692,
+                  "y" => 0.0145984, "z" => 0.0007836, " " => 0.1918182}
     hist,total=freq_hist(raw_string)
-    return total*pearson_chi2(hist,english_freq)
+    return pearson_chi2(hist,english_freq)
   end
   def self.break_xor(cipher_hex)
     key=''
@@ -74,12 +83,12 @@ class CryptoTools
     for c in 32..126
       result = hex_xor_key(cipher_hex,Converters.str_to_hex(c.chr.to_s))
       ascii_res = Converters.hex_to_bytes(result)
-      if ascii_res.scan(/[^[:print:]]/).length > 0
+      if ascii_res.scan(/[^[:print:]]/).length > 1 or
+        Float(ascii_res.scan(/[^[A-Za-z ]]/).length)/ascii_res.length > 0.30
         next
       end
-      score=score_string(ascii_res.scan(/[[:print:]]/).join)
-      puts sprintf "%.4g %s",score,c.chr.to_s
-      if score > 0 and score < minscore
+      score=score_string(ascii_res)
+      if score < minscore
         minscore=score
         key=c.chr.to_s
       end
@@ -160,5 +169,3 @@ class CryptoTools
     return key_str
   end
 end
-
-
