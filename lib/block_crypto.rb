e5b6197330048f621426c35052d4b3c57a10c1f8
David Bjergaard <dbjergaard@gmail.com>
2015-05-19 17:56:40 -0400

    borked cbc encrypt/decrypt

--- lib/block_crypto.rb
+++ lib/block_crypto.rb
@@ -23,14 +23,39 @@ class BlockCrypto
     cipher = OpenSSL::Cipher.new 'AES-128-ECB'
     cipher.encrypt
     cipher.key=key
-    # cipher_text = cipher.update(clear_text) + cipher.final
     # cbc magic
     blocks=[]
     block_size=iv.length
+    # Refactor this to use one loop instead of two
     (Float(clear_text.length)/block_size).ceil().times do |i| 
-      blocks+=[Converters.str_to_hex(clear_text.slice(block_size*i,block_size))]
+      blocks+=[clear_text.slice(block_size*i,block_size)]
     end
-    blocks[-1].pkcs7pad(block_size)
+    blocks[-1]=blocks[-1].pkcs7pad(block_size)
+    firstblock=Converters.hex_to_bytes(CryptoTools.hex_xor(Converters.str_to_hex(blocks.slice!(0)),
+                                                         Converters.str_to_hex(iv)))
+    blocks+=[firstblock];
+    blocks.each.with_index do |block,i| 
+      hex_block=Converters.str_to_hex(block)
+      prev_hex_block=Converters.str_to_hex(blocks[i-1])
+      printf "%s\n",hex_block
+      scrambled=CryptoTools.hex_xor(hex_block,prev_hex_block)
+      blocks[i]=cipher.update(Converters.hex_to_bytes(scrambled))
+    end
+    blocks=[firstblock]+blocks
+    return blocks.join+cipher.final
+  end
+  def self.aes_cbc_decrypt(cipher_text,key,iv)
+    decipher = OpenSSL::Cipher.new 'AES-128-ECB'
+    decipher.decrypt
+    decipher.key=key
+    # cbc magic
+    blocks=[]
+    block_size=iv.length
+    # Refactor this to use one loop instead of two
+    (Float(cipher_text.length)/block_size).ceil().times do |i| 
+      blocks+=[cipher_text.slice(block_size*i,block_size)]
+    end
+    decipher.update(blocks.slice!(0))
     blocks.each.with_index do |block,i| 
       prev=""
       if i==0
@@ -38,17 +63,13 @@ class BlockCrypto
       else
         prev=Converters.str_to_hex(blocks[i-1])
       end
-      scrambled=CryptoTools.hex_xor(block,prev)
-      blocks[i]=Converters.hex_to_bytes(cipher.update(
-                                         Converters.hex_to_bytes(scrambled)))
+      hex_block = Converters.str_to_hex(decipher.update(block))
+      clear_hex=CryptoTools.hex_xor(hex_block,prev)
+      printf "%s\n",clear_hex
+      blocks[i]=Converters.hex_to_bytes(clear_hex)
     end
-    return blocks.join
-  end
-  def self.aes_cbc_decrypt(cipher_text,key,iv)
-    decipher = OpenSSL::Cipher.new 'AES-128-ECB'
-    decipher.decrypt
-    decipher.key=key
-    # cbc magic
-    return decipher.update(cipher_text)+decipher.final
+    # decipher.final may be added to each block.. 
+    return blocks.join+decipher.final
+    # return decipher.update(cipher_text)+decipher.final
   end
 end
