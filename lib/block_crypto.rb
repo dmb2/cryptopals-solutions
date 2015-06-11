db056005ae1b9358a251bf4257abe69495e932dc
David Bjergaard <dbjergaard@gmail.com>
2015-06-11 17:50:13 -0400

    Partial solution to 15

--- lib/block_crypto.rb
+++ lib/block_crypto.rb
@@ -14,11 +14,11 @@ class String
   end
   def pkcs7pad(block_size)
     if block_size > 256
-      raise "PKCS7 is not defined for block sizes larger than 256!"
+      raise EncodingError,"PKCS7 is not defined for block sizes larger than 256!"
     end
     nbytes = block_size - self.length
     if nbytes < 0
-      raise "Trying to pad a string longer than the block size!"
+      raise EncodingError,"Trying to pad a string longer than the block size!"
     end
     str=""
     nbytes.times{
@@ -93,7 +93,6 @@ class BlockCrypto
         break
       end
     end
-    printf "Detected length: %d Actual: %d\n",secret_len,unknown_string.length
     # 2. Detect ECB
     l=CryptoTools.detect_aes_ecb(BlockCrypto.AES_128_ECB(prefix+"A"*256+unknown_string,secret_key))
     if l==""
