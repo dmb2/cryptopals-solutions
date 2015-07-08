4001be3febe8357f983e5d9b214326919cd77037
David Bjergaard <dbjergaard@gmail.com>
2015-07-08 16:06:08 -0400

    Snapshot

--- lib/converters.rb
+++ lib/converters.rb
@@ -21,4 +21,28 @@ class Converters
   def self.base64_to_hex(in_str)
     str_to_hex(Base64.decode64(in_str))
   end
+  # these were adapted from https://stackoverflow.com/questions/9695720/how-do-i-convert-a-64bit-integer-to-a-char-array-and-back
+  def self.str_to_int(str)
+    bytes=str.bytes
+    n = 0
+    table=[0xFF00000000000000,
+           0x00FF000000000000,
+           0x0000FF0000000000,
+           0x000000FF00000000,
+           0x00000000FF000000,
+           0x0000000000FF0000,
+           0x000000000000FF00,
+           0x00000000000000FF]
+    8.times do |i| 
+      n = n | ((bytes[i] << 8*(7-i)) & table[i])
+    end
+    return n
+  end
+  def self.int_to_str(num)
+    bytes=[]
+    8.times do |i| 
+      bytes[i]=(num >> 8*(7-i))
+    end
+    return bytes.pack("C*")
+  end
 end
--- lib/stream_cipher.rb
+++ lib/stream_cipher.rb
@@ -1,8 +1,18 @@
 require 'converters'
 require 'openssl'
 require 'block_crypto'
+
 class StreamCrypto
-  def self.aes_ctr
+  def self.aes_ctr_encrypt(clear_text,nonce,key)
+    if nonce.is_a?(String)==false
+      raise RuntimeError "Nonce is not a string, please use a byte string to initialize the nonce"
+    end
     puts "Hello Dave"
+  end
+  def self.aes_ctr_decrypt(cipher_text,nonce,key)
+    if nonce.is_a?(String)==false
+      raise RuntimeError "Nonce is not a string, please use a byte string to initialize the nonce"
+    end
+    
   end
 end
