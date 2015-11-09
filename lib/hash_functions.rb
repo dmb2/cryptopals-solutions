e14191146feae17c7ce47919955cf087c13e3376
David Bjergaard <dbjergaard@gmail.com>
2015-11-08 21:02:10 -0500

    working sha1

--- lib/hash_functions.rb
+++ lib/hash_functions.rb
@@ -9,9 +9,9 @@ class Hash
   end
   def self.MDpad(message)
     bit_len = message.size << 3
-    message << "\x80".force_encoding('ascii-8bit')
+    message += "\x80".force_encoding('ascii-8bit')
     while (message.size % 64) != 56
-      message << "\0"
+      message += "\0"
     end
     message = message.force_encoding("ascii-8bit") + [bit_len >> 32, bit_len & 0xFFFFFFFF].pack("N2")
     if (message.size % 64)!=0
@@ -53,22 +53,13 @@ class Hash
     end
     return [a,b,c,d,e]
   end
-  def self.sha1(message)
-    # very inefficient, but we won't be using this in any serious application
-    internal_message = message.clone
-    self.MDpad(internal_message)
-    h=[0x67452301,
-       0xEFCDAB89,
-       0x98BADCFE,
-       0x10325476,
-       0xC3D2E1F0]
+  def self.fixed_sha1(message,a,b,c,d,e)
+    # very inefficient, but we won't be using this in any serious
+    # application, MDpad creates a local copy of the message
+    internal_message=self.MDpad(message)
+    h=[a,b,c,d,e]
     internal_message.blocks(64).each do |chunk| 
-      a = h[0]
-      b = h[1]
-      c = h[2]
-      d = h[3]
-      e = h[4]
-      a,b,c,d,e=self.compress_chunk(chunk,a,b,c,d,e)
+      a,b,c,d,e=self.compress_chunk(chunk,h[0],h[1],h[2],h[3],h[4])
       h[0]+=a&0xFFFFFFFF
       h[1]+=b&0xFFFFFFFF
       h[2]+=c&0xFFFFFFFF
@@ -80,6 +71,15 @@ class Hash
       result+=[num].pack("N*")
     end
     return result
+    
+  end
+  def self.sha1(message)
+    return fixed_sha1(message,
+                      0x67452301,
+                      0xEFCDAB89,
+                      0x98BADCFE,
+                      0x10325476,
+                      0xC3D2E1F0)
   end
   
 end
